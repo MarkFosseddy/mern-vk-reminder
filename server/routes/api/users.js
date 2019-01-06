0257ffe67353e7183c5775ff4541dddce53af1f6
MarkFosseddy <markfosseddy@gmail.com>
2019-01-07 01:07:53 +0500

    set up passport

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const UserModel = require('../models/UserModel');
+const UserModel = require('../../models/UserModel');
 
 router.post('/register', (req, res) => {});
 
--- server/server.js
+++ server/server.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 // support .env file
 require('dotenv').config();
 
 const app = express();
 
 // connect Mongo
-const db = require('./config/database').MongoDB;
+const db = require('./config/keys').MongoDB;
 mongoose
 	.connect(
 		db,
 		{ useNewUrlParser: true }
 	)
 	.then(() => console.log('MongoDB connected...'))
-	.catch(err => console.log(err));
+	.catch(err => console.error(err));
 
-// express body parser middleware
+// middleware
+	// express body parser
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+	// passport
+app.use(passport.initialize());
 
 // routes
 const reminders = require('./routes/api/reminders');
-const users = require('./routes/users');
+const users = require('./routes/api/users');
 
 // use routes
 app.use('/api/reminders', reminders);
-app.use('/users/', users);
+app.use('/api/users/', users);
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
