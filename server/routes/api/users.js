af3959815875adcf8bd3423816d4c813944e8f1d
MarkFosseddy <markfosseddy@gmail.com>
2019-01-07 02:27:53 +0500

    add users/register request

--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,9 +1,45 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 
 const UserModel = require('../../models/UserModel');
+const secret = require('../../config/keys').secretOrKey;
 
-router.post('/register', (req, res) => {});
+router.post('/register', (req, res) => {
+	UserModel.findOne({ username: req.body.username })
+		.then(user => {
+			if (user)
+				return res.status(400).json({
+					success: false,
+					message: 'This username is taken'
+				});
+
+			const newUser = new UserModel({
+				username: req.body.username,
+				password: req.body.password,
+				vk: req.body.vk
+			});
+
+			bcrypt.genSalt(10, (err, salt) => {
+				bcrypt.hash(newUser.password, salt, (err, hash) => {
+					if (err) throw err;
+					newUser.password = hash;
+					newUser
+						.save()
+						.then(() =>
+							res.status(200).json({
+								success: true,
+								message: 'User successfully registred'
+							})
+						)
+						.catch(err => console.error(err));
+				});
+			});
+		})
+		.catch(err => console.error(err));
+});
 
 router.post('/login', (req, res) => {});
 
