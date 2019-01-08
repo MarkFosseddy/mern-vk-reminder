66a53bd1f45ff9a3aa6615d8f34fdb89a03990ed
MarkFosseddy <markfosseddy@gmail.com>
2019-01-09 01:24:31 +0500

    add redux boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Footer from './components/layout/Footer';
 import NavBar from './components/layout/NavBar';
@@ -8,21 +9,25 @@ import Register from './components/authentiocation/Registration';
 import Login from './components/authentiocation/Login';
 import Dashboard from './components/Dashboard';
 
+import store from './store';
+
 import './App.css';
 
 class App extends Component {
 	render() {
 		return (
-			<Router>
-				<div>
-					<NavBar />
-					<Route exact path="/" component={ Landing } />
-					<Route path="/register" component={ Register } />
-					<Route path="/login" component={ Login } />
-					<Route path="/dashboard" component={ Dashboard } />
-					<Footer />
-				</div>
-			</Router>
+			<Provider store={ store }>
+				<Router>
+					<div>
+						<NavBar />
+						<Route exact path="/" component={ Landing } />
+						<Route path="/register" component={ Register } />
+						<Route path="/login" component={ Login } />
+						<Route path="/dashboard" component={ Dashboard } />
+						<Footer />
+					</div>
+				</Router>
+			</Provider>
 		);
 	}
 }
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,12 @@
+import { SET_CURRENT_USER } from '../actions/types';
+
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+const authReducer = (state = initialState, action) => {
+
+};
+
+export default authReducer;
