81e2c295856674c22039fe026b5f73d3cc6db069
MarkFosseddy <markfosseddy@gmail.com>
2019-01-12 14:41:22 +0500

    update routes

--- server/routes/api/reminders.js
+++ server/routes/api/reminders.js
@@ -2,107 +2,38 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 
-const ReminderModel = require('../../models/ReminderModel');
+const reminderController = require('../../controllers/reminderController');
 
 // @route   GET api/reminders/
-// @desc    get reminders
 // @access  Private
 router.get(
   '/',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    ReminderModel.find({ user_id: req.user.id })
-      .sort({ date: -1 })
-      .then(reminders => res.status(200).json(reminders))
-      .catch(() =>
-        res.status(404).json({
-          success: false,
-          message: 'Something went wrong'
-        })
-      );
-  }
+  reminderController.getReminders
 );
 
 // @route   POST api/reminders/
-// @desc    add new reminder
 // @access  Private
 router.post(
   '/',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    const newReminder = new ReminderModel({
-      user_id: req.user.id,
-      text: req.body.text,
-      whenToRemind: req.body.whenToRemind
-    });
-    newReminder
-      .save()
-      .then(() => res.status(200).json(newReminder))
-      .catch(() =>
-        res.status(400).json({
-          success: false,
-          message: 'Input must not be empty'
-        })
-      );
-  }
+  reminderController.addReminder
 );
 
 // @route   PUT api/reminders/:id
-// @desc    update reminder
 // @access  Private
 router.put(
   '/:id',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    ReminderModel.findOne({ _id: req.params.id })
-      .then(reminder => {
-        reminder.text = req.body.text;
-        reminder.whenToRemind = req.body.whenToRemind;
-        reminder
-          .save()
-          .then(() => res.status(200).json(reminder))
-          .catch(() =>
-            res.status(400).json({
-              success: false,
-              message: 'Input must not be empty'
-            })
-          );
-      })
-      .catch(() =>
-        res.status(404).json({
-          success: false,
-          message: 'Reminder does not exist'
-        })
-      );
-  }
+  reminderController.updateReminder
 );
 
 // @route   DELETE api/reminders/:id
-// @desc    delete user reminder
 // @access  Private
 router.delete(
   '/:id',
   passport.authenticate('jwt', { session: false }),
-  (req, res) => {
-    ReminderModel.findOne({ _id: req.params.id })
-      .then(reminder =>
-        reminder
-          .remove()
-          .then(() =>
-            res.status(200).json({
-              success: true,
-              message: 'Reminder successfully removed'
-            })
-          )
-          .catch(err => console.error(err))
-      )
-      .catch(() =>
-        res.status(404).json({
-          success: false,
-          message: 'Reminder does not exist'
-        })
-      );
-  }
+  reminderController.deleteReminder
 );
 
 module.exports = router;
--- server/routes/api/users.js
+++ server/routes/api/users.js
@@ -1,88 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
 
-const UserModel = require('../../models/UserModel');
-const secret = require('../../config/keys').secretOrKey;
+const userController = require('../../controllers/userController');
 
 // @route   POST api/users/register
 // @desc    register user
 // @access  Public
-router.post('/register', (req, res) => {
-  UserModel.findOne({ username: req.body.username }).then(user => {
-    if (user) {
-      return res.status(400).json({
-        success: false,
-        message: 'This username is already taken'
-      });
-    }
-
-    const newUser = new UserModel({
-      username: req.body.username,
-      password: req.body.password,
-      vk: req.body.vk
-    });
-
-    bcrypt.genSalt(10, (err, salt) => {
-      if (err) throw err;
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then(() =>
-            res.status(200).json({
-              success: true,
-              message: 'User successfully registred'
-            })
-          )
-          .catch(() =>
-            res.status(400).json({
-              success: false,
-              message: 'Please fill all fields'
-            })
-          );
-      });
-    });
-  });
-});
+router.post('/register', userController.register);
 
 // @route   POST api/users/login
 // @desc    login user
 // @access  Public
-router.post('/login', (req, res) => {
-  UserModel.findOne({ username: req.body.username }).then(user => {
-    if (!user) {
-      return res.status(400).json({
-        success: false,
-        message: 'User does not exist'
-      });
-    }
-
-    bcrypt
-      .compare(req.body.password, user.password)
-      .then(() => {
-        const payload = {
-          id: user.id,
-          username: user.username,
-          vk: user.vk
-        };
-        jwt.sign(payload, secret, { expiresIn: '1h' }, (err, token) =>
-          res.status(200).json({
-            success: true,
-            message: 'You are successfully logged in',
-            jwtToken: `Bearer ${token}`
-          })
-        );
-      })
-      .catch(() =>
-        res.status(400).json({
-          success: false,
-          message: 'Incorrect password'
-        })
-      );
-  });
-});
+router.post('/login', userController.login);
 
 module.exports = router;
