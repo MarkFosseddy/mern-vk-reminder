0613b30e71c55d388c8dd1e5640854e0417cb437
MarkFosseddy <markfosseddy@gmail.com>
2019-01-14 12:13:38 +0500

    change from then() to async/await

--- server/agenda/jobs.js
+++ server/agenda/jobs.js
@@ -1,6 +1,22 @@
+const vk = require('./vk');
+
+const UserModle = require('../models/UserModel');
+const ReminderModel = require('../models/ReminderModel');
+
 module.exports = agenda => {
-  agenda.define('send reminder', job => {
-    console.log(job.attrs.data.text);
-    agenda.cancel({ data: job.attrs.data });
+  agenda.define('send reminder', async job => {
+    try {
+      const user = await UserModle.findById(job.attrs.data.reminder_user_id);
+      await vk.sendReminder(job.attrs.data.text, user.vk);
+    
+      const reminder = await ReminderModel.findById(job.attrs.data.reminder_id);
+      reminder.isCompleted = true;
+      await reminder.save();
+
+      agenda.cancel({ data: job.attrs.data });
+
+    } catch (err) {
+      console.error(err);
+    }
   })
 };
