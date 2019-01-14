4b3915b72f847561add87ef1aab83bda363c9a9b
MarkFosseddy <markfosseddy@gmail.com>
2019-01-14 21:44:19 +0500

    add user model methods

--- server/models/UserModel.js
+++ server/models/UserModel.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { secretOrKey } = require('../config/keys');
+
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -20,6 +24,40 @@ const UserSchema = new Schema({
     trim: true
   }
 });
+
+UserSchema.methods.encryptPassword = 
+  async function() {
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hash = await bcrypt.hash(this.password, salt);
+      this.password = hash;
+      this.save();
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+UserSchema.methods.comparePasswords = 
+  async function(password) {
+    try {
+      const isMatch = await bcrypt.compare(password, this.password);
+      return isMatch;
+
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+UserSchema.methods.generateJwt = function() {
+  const payload = {
+    id: this._id,
+    username: this.username,
+    vk: this.vk
+  };
+  
+  return jwt.sign(payload, secretOrKey, { expiresIn: '1h' });
+};
 
 const UserModel = mongoose.model('User', UserSchema);
 module.exports = UserModel;
