ea052669fb7d3af9bb69af0e61f07a273597102c
MarkFosseddy <markfosseddy@gmail.com>
2019-01-14 12:12:33 +0500

    add sending reminders to vk messages

--- server/agenda/vk.js
+++ server/agenda/vk.js
@@ -1,1 +1,51 @@
-'https://oauth.vk.com/authorize?client_id=6817510&display=page&redirect_uri=https://oauth.vk.com/blank.html&scope=friends&response_type=token&v=5.52'
+const axios = require('axios');
+const { accessTokenVK } = require('../config/keys');
+const apiVersion = 5.92;
+
+const getRandomInt32 = () => Math.floor(Math.random() * (2147483647 - 1) + 1);
+const randomInt32 = getRandomInt32();
+
+const getUsername = VKLink => VKLink.replace('https://vk.com/', '');
+const getUserId = VKLink => VKLink.replace('https://vk.com/id', '');
+
+// returns NaN if string or int(id)
+const parseVKLink = VKLink =>
+  parseInt(
+    getUsername(VKLink) && getUserId(VKLink)
+  );
+
+const getUserIdFromVKLink = VKLink => 
+  parseVKLink(VKLink)
+    ? getUserId(VKLink)
+    : getUserIdFromUsername(getUsername(VKLink));
+
+const getUserIdFromUsername = async nameCase => {
+  try {
+    const res = await axios.get(`https://api.vk.com/method/users.get?name_case=${nameCase}&access_token=${accessTokenVK}&v=${apiVersion}`);
+    const id = await res.data.response[0].id;
+    return id;
+
+  } catch(err) {
+    console.error(err);
+  }  
+}
+
+const sendMessage = async (message, userId) => { 
+  try {
+    // encodeURI потому что node не дружит с кириллицей
+    await axios.get(encodeURI(`https://api.vk.com/method/messages.send?user_id=${userId}&random_id=${randomInt32}&message=${message}&access_token=${accessTokenVK}&v=${apiVersion}`));
+
+  } catch(err) {
+    console.error(err);
+  }
+};
+
+exports.sendReminder = async (message, VKLink) => {
+  try {
+    const id = await getUserIdFromVKLink(VKLink);
+    await sendMessage(message, id);
+
+  } catch (err) {
+    console.error(err);
+  }
+};
