2877482b66df1db4a146da5ff1232f6d67b10ce0
MarkFosseddy <markfosseddy@gmail.com>
2019-01-15 17:03:35 +0500

    moved server

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const passport = require('passport');
+const path = require('path');
 // passport config
 require('./config/passport')(passport);
 
@@ -30,6 +31,14 @@ const users = require('./routes/api/users');
 // use routes
 app.use('/api/reminders', reminders);
 app.use('/api/users', users);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Listening to port ${port}`));
