da685220b9075d3322964fe144dd58e8f66f81f3
MarkFosseddy <markfosseddy@gmail.com>
2019-01-15 17:34:46 +0500

    deploy

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoDB: process.env.MONGO_DB,
-  secretOrKey: process.env.SECRET_OR_KEY,
-  accessTokenVK: process.env.ACCESS_TOKEN_VK
+  mongoDB: 'mongodb://Fosseddy:test1234@ds149344.mlab.com:49344/vk-reminder',
+  secretOrKey: 'cosmos',
+  accessTokenVK: 'c2ad3ec1658899f0fd4a7fd6851b9fd520c8e60c032a45c51aa94c7262cd03a71d0c1aa6140d4c3a23034'
 };
