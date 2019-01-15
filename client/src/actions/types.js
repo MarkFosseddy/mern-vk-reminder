cab945c09ba5d259e25c24aa2a5363b75317443d
MarkFosseddy <markfosseddy@gmail.com>
2019-01-15 16:16:13 +0500

    remove update constant

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -4,7 +4,6 @@ export const LOGOUT_USER = 'LOGOUT_USER';
 export const GET_REMINDERS = 'GET_REMINDERS';
 export const ADD_REMINDER = 'ADD_REMINDER';
 export const DELETE_REMINDER = 'DELETE_REMINDER'
-export const UPDATE_REMINDER = 'UPDATE_REMINDER'
 
 export const GET_ERRORS = 'GET_ERRORS';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
